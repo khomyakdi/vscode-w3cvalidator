59655d3b8e14af043f87e889961243821e86f67a
khomiakdi <khomyakdi@gmail.com>
2019-01-01 00:59:50 +0200

    added getting text from opened editor

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -1,4 +1,18 @@
 import * as vscode from 'vscode';
+const URL: string = "https://validator.w3.org/nu/?out=json";
 export const validateHtml = function () {
-    vscode.window.showInformationMessage("validator is started");
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showInformationMessage("Please open any editor window");
+        return;
+    }
+    console.log(getTextFromEditor(editor));
 };
+function getTextFromEditor (textEditor: any): string {
+    let text =  textEditor.document.getText();
+    const selection = textEditor.selection;
+    if(selection.start.line !== selection.end.line ||  selection.start.character !== selection.end.character) {
+        text = textEditor.document.getText(selection);
+    }
+   return text;
+}

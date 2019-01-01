c7ab64ab268370b4d4a1d3e4674a3af55cf879d4
khomiakdi <khomyakdi@gmail.com>
2019-01-01 02:11:53 +0200

    added getting validation result from API

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -1,12 +1,15 @@
 import * as vscode from 'vscode';
-const URL: string = "https://validator.w3.org/nu/?out=json";
+import * as request from 'request';
+const API_URL: string = "https://validator.w3.org/nu/?out=json";
 export const validateHtml = function () {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
         vscode.window.showInformationMessage("Please open any editor window");
         return;
     }
-    console.log(getTextFromEditor(editor));
+    getValidationFromApi(getTextFromEditor(editor))
+    .then( data => console.log(data))
+    .catch(statusCode => console.log('error: '+statusCode));
 };
 function getTextFromEditor (textEditor: any): string {
     let text =  textEditor.document.getText();
@@ -15,4 +18,24 @@ function getTextFromEditor (textEditor: any): string {
         text = textEditor.document.getText(selection);
     }
    return text;
+}
+function getValidationFromApi (text: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        request.post( {
+            headers: { 'Content-Type': 'text/html',
+                'User-Agent': 'vscode'
+            },
+            url: API_URL,
+            body: text
+        },
+        function(error,response, body) {
+            
+            if(response.statusCode === 200) {
+                resolve(JSON.parse(body));
+            }
+            else {
+                reject(response.statusCode);
+            }
+        });
+    });
 }
